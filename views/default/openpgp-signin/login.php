4b1011772e5963b92c66a84248d5fb98b6d6ed50
Marcus Povey <marcus@marcus-povey.co.uk>
2014-05-28 17:25:49 +0100

    Login with submit

---
+++ views/default/openpgp-signin/login.php
@@ -0,0 +1,21 @@
+<div class="row">
+    <form id="pgp-login" action="<?= $vars['return_url']; ?>" method="post">
+	<div class="control-group">
+	    <div class="controls">
+		<p>
+		    Relax, we're logging you in right now...
+		</p>
+		<div class="control-group">
+		    <div class="controls">
+			<textarea id="signature" name="signature" class="span4" style="display:none;"><?= htmlspecialchars($vars['signature']); ?></textarea>
+		    </div>
+		</div>
+	    </div>
+	</div>
+    </form>
+</div>
+<script>
+    $(document).ready(function() {
+	$('#pgp-login').submit();
+    })
+</script>
