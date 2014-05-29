392ffd333944b68b4f62eadb1d27c85213a42604
Marcus Povey <marcus@marcus-povey.co.uk>
2014-05-29 13:08:49 +0100

    Save remote user public key

--- start.php
+++ start.php
@@ -14,13 +14,42 @@ elgg_register_event_handler('init', 'system', function() {
 
     // Register libraries
     elgg_register_library('openpgp-signin', dirname(__FILE__) . '/lib/openpgp-signin.php');
-    
+
     // Register javascript
     elgg_register_js('openpgpsignin', elgg_get_site_url() . 'mod/elgg-openpgp-signin/vendor/openpgp.min.js');
-    
+
     // Extend header
     elgg_extend_view('page/elements/head', 'openpgp-signin/header');
-    
+
+    // Register friend
+    elgg_register_event_handler('create', 'friend', function ($event, $type, $object) {
+
+	if ($user_two = get_entity($object->guid_two)) {
+	    
+	    elgg_load_library('openpgp-signin');
+	    
+	    if ($publickey = findPublicKey($user_two->getUrl())) {
+		
+		// Save it to the keyring
+		$gpg = new \gnupg();
+		$result = $gpg->import($publickey);
+
+		// Save a signature against the user
+		if ($result && isset($result['fingerprint'])) {
+		    error_log("Imported public key, with fingerprint {$result['fingerprint']}");
+
+		    // Save against following user
+
+		    $user_two->pgp_publickey_fingerprint = $result['fingerprint'];
+		} else {
+		    error_log("Key data could not be imported");
+		}
+	    }
+	    
+	}
+	
+    });
+
     // Signature specified on any page, grab it and save it
     if (isset($_REQUEST['signature'])) {
 	error_log("Ooo... we have a signature, saving in session for later...");
@@ -74,7 +103,7 @@ elgg_register_event_handler('init', 'system', function() {
 	// RegisterPages doesn't have a default exception handler, so lets do something with error messages
 	register_error($e->getMessage());
     }
-    
+
     // Register bookmark page handler
     elgg_register_page_handler('openpgpsignin', function($page) {
 
@@ -84,51 +113,56 @@ elgg_register_event_handler('init', 'system', function() {
 	    elgg_load_js('openpgpsignin');
 
 	    switch ($page[0]) {
-		
+
 		case 'login' :
-		    
+
 		    $returnURL = getInput('u');
-		
+
 		    $user = elgg_get_logged_in_user_entity();
-		
-		    if (!$returnURL) throw new \Exception ('You need to send a return URL!');
-		    if (!$user) throw new \Exception ('No user, this shouldn\'t happen');
+
+		    if (!$returnURL)
+			throw new \Exception('You need to send a return URL!');
+		    if (!$user)
+			throw new \Exception('No user, this shouldn\'t happen');
 
 		    // Ok, we have a user and a return URL, so lets encrypt and sign the url, and forward back passing the message back as the variable "key" with "u"
 		    $gpg = new \gnupg();
 
-		    if (!$gpg->addsignkey($user->pgp_privatekey_fingerprint, '')) throw new \Exception('There was a problem adding the signing key, have you set your keypair?','');
+		    if (!$gpg->addsignkey($user->pgp_privatekey_fingerprint, ''))
+			throw new \Exception('There was a problem adding the signing key, have you set your keypair?', '');
 
 		    $signature = $gpg->sign($user->getUrl());
-		    if (!$signature) throw new \Exception('There was a problem signing: ' . $gpg -> geterror());
-		
-		
+		    if (!$signature)
+			throw new \Exception('There was a problem signing: ' . $gpg->geterror());
+
+
 		    // Render it and trigger a submit back
 		    $body = elgg_view('openpgp-signin/login', ['signature' => $signature, 'user' => $user->getUrl(), 'return_url' => $returnURL]);
-                    
+
 		    echo elgg_view_page('Logging in...', elgg_view_layout('content', array(
 			'content' => $body,
 			'title' => 'Logging in...',
 			'filter' => ''
 		    )));
-		    
-		break;
-		
+
+		    break;
+
 		case 'key' :
 		    set_input('username', $page[1]);
-		    
+
 		    $user = get_user_by_username($page[1]);
 		    if ($user) {
 			if ($key = elgg_get_plugin_user_setting('public_key', $user->guid, 'elgg-openpgp-signin')) {
 			    header('Content-Type: text/plain');
 
-			    echo $key; exit;
+			    echo $key;
+			    exit;
 			}
 		    }
-		    
+
 		    echo "No key for {$page[1]}";
 		    http_response_code(404);
-		    
+
 		    exit;
 		    break;
 	    }
